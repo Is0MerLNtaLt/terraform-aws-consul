09637d660e9acde710940bef78655f39d4d7a194
G <54684207+codegabru@users.noreply.github.com>
2020-06-05 00:01:23 +0530

    feat: add instance_profile_id output

--- modules/consul-cluster/outputs.tf
+++ modules/consul-cluster/outputs.tf
@@ -38,3 +38,8 @@ output "cluster_tag_value" {
   description = "This is the tag value used to allow the consul servers to autojoin"
 }
 
+output "instance_profile_id" {
+  value = aws_iam_instance_profile.instance_profile.id
+}
+
+
